06d777efb8c60d8b411da8869341806fa616ca57
alrizaldi <10rizaldiakbar@gmail.com>
2019-01-07 15:55:14 +0700

    commit pertama gan

---
+++ src/program/penjualan/kaca/mata/ProgramPenjualanKacaMata.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program.penjualan.kaca.mata;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class ProgramPenjualanKacaMata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_penjualan_kaca_mata Laund = new Frame_penjualan_kaca_mata();
+        Laund.setVisible(true);
+    }
+}
